23402fbf5ae0637c5957a40348e523e04ce69f06
RoneyThomas <roneythomas6@gmail.com>
2022-12-27 00:22:26 -0500

    Added two databinding so now the project follows mvvm

--- app/src/main/java/me/roney/courtcounter/MainActivity.java
+++ app/src/main/java/me/roney/courtcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package me.roney.courtcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,64 +10,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import me.roney.courtcounter.databinding.ActivityMainBinding;
 
-    private ScoreViewModel mViewModel;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mViewModel = new ViewModelProvider(this).get(ScoreViewModel.class);
-        mViewModel.scoreTeamA.observe(this, this::displayForTeamA);
-        mViewModel.scoreTeamB.observe(this, this::displayForTeamB);
-    }
-
-    public void addThreeForTeamA(View view) {
-        mViewModel.scoreTeamA.setValue(mViewModel.scoreTeamA.getValue() + 3);
-    }
-
-    public void addTwoForTeamA(View view) {
-        mViewModel.scoreTeamA.setValue(mViewModel.scoreTeamA.getValue() + 2);
-    }
-
-    public void addOneForTeamA(View view) {
-        mViewModel.scoreTeamA.setValue(mViewModel.scoreTeamA.getValue() + 1);
-    }
-
-    public void addThreeForTeamB(View view) {
-        mViewModel.scoreTeamB.setValue(mViewModel.scoreTeamB.getValue() + 3);
-//        displayForTeamB(mViewModel.scoreTeamB);
-    }
-
-    public void addTwoForTeamB(View view) {
-        mViewModel.scoreTeamB.setValue(mViewModel.scoreTeamB.getValue() + 2);
-//        displayForTeamB(mViewModel.scoreTeamB);
-    }
-
-    public void addOneForTeamB(View view) {
-        mViewModel.scoreTeamB.setValue(mViewModel.scoreTeamB.getValue() + 1);
-//        displayForTeamB(mViewModel.scoreTeamB);
-    }
-
-    /**
-     * Displays the given score for Team A.
-     */
-    public void displayForTeamA(int score) {
-        TextView scoreView = findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-    /**
-     * Displays the given score for Team B.
-     */
-    public void displayForTeamB(int score) {
-        TextView scoreView = findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
-    }
-
-    public void resetScore(View view) {
-        mViewModel.scoreTeamA.setValue(0);
-        mViewModel.scoreTeamB.setValue(0);
+        ScoreViewModel mViewModel = new ViewModelProvider(this).get(ScoreViewModel.class);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setScoreViewModel(mViewModel);
+        binding.setLifecycleOwner(this);
     }
 }
--- app/src/main/java/me/roney/courtcounter/ScoreViewModel.java
+++ app/src/main/java/me/roney/courtcounter/ScoreViewModel.java
@@ -1,5 +1,7 @@
 package me.roney.courtcounter;
 
+import android.view.View;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,4 +9,20 @@ import androidx.lifecycle.ViewModel;
 public class ScoreViewModel extends ViewModel {
     public MutableLiveData<Integer> scoreTeamA = new MutableLiveData<>(0);
     public MutableLiveData<Integer> scoreTeamB = new MutableLiveData<>(0);
+
+    public void resetScore() {
+        scoreTeamA.setValue(0);
+        scoreTeamB.setValue(0);
+    }
+
+    public void addPoint(String team, int score) {
+        switch (team) {
+            case "Team A":
+                scoreTeamA.setValue(scoreTeamA.getValue() + score);
+                break;
+            case "Team B":
+                scoreTeamB.setValue(scoreTeamB.getValue() + score);
+                break;
+        }
+    }
 }
