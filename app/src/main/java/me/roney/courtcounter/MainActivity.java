30314b29ea1afb485690307c4b130da25342e9ee
RoneyThomas <roneythomas6@gmail.com>
2022-12-28 06:48:40 -0500

    Material 3 theme

--- app/src/main/java/me/roney/courtcounter/MainActivity.java
+++ app/src/main/java/me/roney/courtcounter/MainActivity.java
@@ -2,13 +2,12 @@ package me.roney.courtcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+
+import com.google.android.material.elevation.SurfaceColors;
 
 import me.roney.courtcounter.databinding.ActivityMainBinding;
 
@@ -17,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Make statusbar have same color as actionbar
+        getWindow().setStatusBarColor(SurfaceColors.SURFACE_2.getColor(this));
         setContentView(R.layout.activity_main);
         ScoreViewModel mViewModel = new ViewModelProvider(this).get(ScoreViewModel.class);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
